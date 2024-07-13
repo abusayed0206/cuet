cfad1a445e0ca00a7fea0687e8bc50474584f421
MD. ABU SAYED <hello@sayed.page>
2024-07-14 01:40:43 +0600

    Create page.tsx

---
+++ src/app/dark/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+import {
+  Card,
+  CardTitle,
+  CardDescription,
+  CardHeader,
+  CardContent,
+} from "@/components/ui/card";
+import ParticleBackground from "@/components/ui/Particle";
+import { getDate, getWeekDay } from "bangla-calendar";
+import { BengaliDate } from "to-bengali";
+import { useState, useEffect } from "react";
+
+interface Quote {
+  id: number;
+  author: string;
+  quote: string;
+  series: string;
+}
+
+export default function DarkQuotePage() {
+  const banglaDate = getDate(new Date(), {
+    format: "D MMMM, YYYY",
+    calculationMethod: "BD",
+  });
+  const banglaWeekDay = getWeekDay(new Date(), {
+    format: "eeee",
+    calculationMethod: "BD",
+  });
+  const [currentDate, setCurrentDate] = useState<Date | null>(null);
+  const [quote, setQuote] = useState<Quote | null>(null);
+
+  useEffect(() => {
+    setCurrentDate(new Date());
+
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  useEffect(() => {
+    fetch("https://api.seriesquotes.10cyrilc.me/quote/?series=dark&count=1&all=false")
+      .then((response) => response.json())
+      .then((data) => setQuote(data[0]))
+      .catch((error) => console.error("Error fetching quote:", error));
+  }, []);
+
+  if (!currentDate) {
+    return null; // or a loading spinner
+  }
+
+  const banglaDateTime = new BengaliDate(currentDate).format("date");
+  const banglaTime = new BengaliDate(currentDate).format("AAAA hh:mm:ss");
+
+  return (
+    <div className="relative w-full h-screen overflow-hidden flex items-center justify-center">
+      <div className="absolute inset-0 z-0">
+        <ParticleBackground />
+      </div>
+      <div className="relative z-10 w-full h-full flex flex-col items-center justify-center">
+        <div className="mx-6 rounded-2xl max-w-screen-md bg-white text-black">
+          <Card className="bg-white text-black">
+            <CardDescription className="p-2 border-b">
+              <p className="text-sm text-center">
+                {banglaWeekDay} | {banglaTime}{" "}
+              </p>
+              <p className="text-sm text-center">
+                {banglaDate} | {banglaDateTime}
+              </p>
+            </CardDescription>
+            <CardTitle className="p-6">
+              {quote ? (
+                <div className="text-center">
+                  <p className="text-xl font-bold">&quot;{quote.quote}&quot;</p>
+                </div>
+              ) : (
+                <p className="text-center">Fetching quote...</p>
+              )}
+            </CardTitle>
+            {quote && (
+              <CardContent className="text-center">
+                <p className="text-base italic mt-2">
+                  - {quote.author ? quote.author : "Unknown"}
+                </p>
+              </CardContent>
+            )}
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
