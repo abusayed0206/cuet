68f1fd1dedcf86d60cd0f464d2aa5402fc167c44
MD. ABU SAYED <hello@sayed.page>
2024-07-14 17:37:58 +0600

    Update page.tsx

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import DateObject from "react-date-object";
 import { AvatarImage, AvatarFallback, Avatar } from "@/components/ui/avatar";
-import { CiBarcode } from "react-icons/ci";
+import { GrLinkedin } from "react-icons/gr";
 import { HiDownload } from "react-icons/hi";
 import ParticleBackground from "@/components/ui/Particle";
 import {
@@ -127,11 +127,11 @@ export default function Home() {
               </Link>
               <Link
                 className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition ml-4"
-                href="/hire"
+                href="/linekdin"
                 target="_blank"
               >
                 Hire Me
-                <CiBarcode className="opacity-100 group-hover:translate-y-1 transition" />
+                <GrLinkedin className="opacity-100 group-hover:translate-y-1 transition" />
               </Link>
             </CardFooter>
           </Card>
