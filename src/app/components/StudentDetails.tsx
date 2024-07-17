8f5bf7c663a17720abbc5da54fc60c60c253e7a7
MD. ABU SAYED <hello@sayed.page>
2024-07-18 00:10:12 +0600

    Update StudentDetails.tsx

--- src/app/components/StudentDetails.tsx
+++ src/app/components/StudentDetails.tsx
@@ -38,19 +38,19 @@ const StudentDetails: React.FC<StudentDetailsProps> = ({ data }) => {
           <p className="font-bold text-gray-700">LinkedIn:</p>
         </div>
         <div>
-          <p>{data.name}</p>
-          <p>{data.studentid}</p>
-          <p>{data.uniqueid}</p>
-          <p>{data.batch}</p>
-          <p>{data.session}</p>
-          <p>{data.department}</p>
-          <p>{data.admissionroll}</p>
-          <p>{data.admissionmerit}</p>
-          <p>{data.hall}</p>
-          <p>{data.email ? <a href={`mailto:${data.email}`} className="text-blue-500">{data.email}</a> : "Not Added Yet"}</p>
-          <p>{data.phonenumber}</p>
-          <p>{data.bloodgroup}</p>
-          <p>{data.linkedin ? <a href={data.linkedin} className="text-blue-500" target="_blank" rel="noopener noreferrer">LinkedIn Profile</a> : "Not Added Yet"}</p>
+          <p className="text-black">{data.name}</p>
+          <p className="text-black">{data.studentid}</p>
+          <p className="text-black">{data.uniqueid}</p>
+          <p className="text-black">{data.batch}</p>
+          <p className="text-black">{data.session}</p>
+          <p className="text-black">{data.department}</p>
+          <p className="text-black">{data.admissionroll}</p>
+          <p className="text-black">{data.admissionmerit}</p>
+          <p className="text-black">{data.hall}</p>
+          <p className="text-black">{data.email ? <a href={`mailto:${data.email}`} className="text-blue-500">{data.email}</a>}</p>
+          <p className="text-black">{data.phonenumber}</p>
+          <p className="text-black">{data.bloodgroup}</p>
+          <p className="text-black">{data.linkedin && <a href={data.linkedin} className="text-blue-500" target="_blank" rel="noopener noreferrer">Profile</a>}</p>
         </div>
       </div>
     </div>
