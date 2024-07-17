331337df4e4fb818596ca68724c78c9449be87ed
MD. ABU SAYED <hello@sayed.page>
2024-07-17 16:26:54 +0600

    Delete src/app directory

--- src/app/types/turnstile.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="turnstile-types" />
-
-declare global {
-    interface Window {
-      turnstile?: Turnstile.Turnstile;
-    }
-  }
-  
-  declare const turnstile: Turnstile.Turnstile;
