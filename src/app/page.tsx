f8c2d9b45e23f1e86f8324fb54b8888561e10fa2
MD. ABU SAYED <hello@sayed.page>
2024-07-17 22:37:39 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
                   />
                   <label htmlFor="studentId" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Student ID</label>
                 </div>
-                <div className="relative">
+                <div className="relative flex justify-center">
                   <button className="bg-gradient-to-r from-purple-500 to-indigo-600 text-white rounded-md px-4 py-2 hover:from-pink-500 hover:to-yellow-500 transition-all">Submit</button>
                 </div>
               </form>
