8ed1bbea6dd548496922a4c0645549b22f1cfe6c
MD. ABU SAYED <hello@sayed.page>
2024-07-18 00:17:15 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     try {
       const response = await fetch(`/api/student/${studentId}`)
       if (!response.ok) {
-        throw new Error('He/She is not a CUET Student'😓)
+        throw new Error('He/She is not a CUET Student')
       }
       const data = await response.json()
       setStudentData(data)
@@ -34,7 +34,7 @@ export default function Home() {
         <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
             <div>
-              <h1 className="text-3xl font-semibold text-center text-gray-800 mb-8">CUET Student Information</h1>
+              <h1 className="text-3xl font-semibold text-center text-grayI-8">CUET Student Information</h1>
             </div>
             <div className="divide-y divide-gray-200">
               <form onSubmit={handleSubmit} className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
@@ -43,12 +43,12 @@ export default function Home() {
                     id="studentId" 
                     name="studentId" 
                     type="text" 
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600 justify-center" 
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600" 
                     placeholder="Student ID" 
                     value={studentId}
                     onChange={(e) => setStudentId(e.target.value)}
                   />
-                  <label htmlFor="studentId" className="absolute center-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Student ID</label>
+                  <label htmlFor="studentId" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Student ID</label>
                 </div>
                 <div className="relative flex justify-center">
                   <button className="bg-gradient-to-r from-purple-500 to-indigo-600 text-white rounded-md px-4 py-2 hover:from-pink-500 hover:to-yellow-500 transition-all">Submit</button>
