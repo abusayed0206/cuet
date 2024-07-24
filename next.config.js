14973644266cbb38a813048c4b1a330758648621
MD. ABU SAYED <hello@sayed.page>
2024-07-25 01:19:43 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -19,4 +19,10 @@ module.exports = {
       },
     ],
   },
+  env: {
+    R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
+    R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
+    R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
+    R2_BUCKET: process.env.R2_BUCKET,
+  },
 };
