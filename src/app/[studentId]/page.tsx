794d57687719068c92a3cf373cf2346a2f0cbe10
MD. ABU SAYED <hello@sayed.page>
2024-07-29 21:17:39 +0600

    Update page.tsx

--- src/app/[studentId]/page.tsx
+++ src/app/[studentId]/page.tsx
@@ -64,10 +64,8 @@ const StudentPage = ({ params }: { params: { studentId: string } }) => {
         <div className="absolute inset-0 bg-gradient-to-r from-teal-400 to-cyan-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
         <div className="relative px-4 py-10 bg-slate-300 shadow-lg sm:rounded-3xl sm:p-20">
           <div className="max-w-md mx-auto">
-            <div>
-              <h1 className="text-2xl font-semibold text-center text-black">CUET Student Information</h1>
-            </div>
-            {isLoading && <p className="text-black text-center">Loading CUETian information...</p>}
+            
+            {isLoading && <p className="text-black text-center">CUET: Centre of Excellency</p>}
             {error && <p className="text-center text-red-500">{error}</p>}
             {studentData && <StudentDetails data={studentData} />}
           </div>
