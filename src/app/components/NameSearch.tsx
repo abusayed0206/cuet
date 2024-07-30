f30f9c9debe3f9a87474c0014b63e7e528a6829e
MD. ABU SAYED <hello@sayed.page>
2024-07-30 21:36:06 +0600

    Update NameSearch.tsx

--- src/app/components/NameSearch.tsx
+++ src/app/components/NameSearch.tsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useState } from 'react';
+import React from 'react';
 
 interface Student {
   name: string;
@@ -15,14 +14,14 @@ interface NameSearchProps {
 
 const NameSearch: React.FC<NameSearchProps> = ({ results }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 p-6">
       {results.map((student) => (
-        <div key={student.studentid} className="bg-white rounded-lg shadow-lg overflow-hidden">
+        <div key={student.studentid} className="bg-white rounded-lg shadow-lg overflow-hidden flex flex-col items-center text-center">
           <div className="relative">
             <img
               src={student.dplink}
               alt={`${student.name}'s DP`}
-              className="w-full h-48 object-cover rounded-t-lg"
+              className="w-24 h-24 object-cover rounded-full mt-4"
               onError={(e) => (e.currentTarget.src = 'https://cdn.abusayed.dev/demo.png')}
             />
             <a
@@ -34,11 +33,16 @@ const NameSearch: React.FC<NameSearchProps> = ({ results }) => {
               ID: {student.studentid}
             </a>
           </div>
-          <div className="p-4">
-            <h2 className="text-xl font-bold mb-2">{student.name}</h2>
+          <div className="p-4 flex flex-col items-center">
+            <a
+              href={`https://cuet.sayed.page/${student.studentid}`}
+              className="text-xl font-bold text-black mb-2"
+            >
+              {student.name}
+            </a>
+            <p className="text-gray-600">Batch: {student.batch}</p>
             <p className="text-gray-600 mb-1">{student.department}</p>
-            <p className="text-gray-600">{student.batch}</p>
-          </div>
+            </div>
         </div>
       ))}
     </div>
