1395b2d22d149730dbc7dba725137315f9ade8a3
MD. ABU SAYED <hello@sayed.page>
2024-08-01 09:36:37 +0600

    Update page.tsx

--- src/app/dpupload/page.tsx
+++ src/app/dpupload/page.tsx
@@ -79,7 +79,7 @@ export default function UploadPage() {
               value={id}
               onChange={(e) => setId(e.target.value)}
               required
-              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+              className="mt-1 text-black block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
             />
           </div>
           <div>
