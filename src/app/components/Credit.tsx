821147f052d6c6c17486eeed8b7c2d3f5e62604b
MD. ABU SAYED <hello@sayed.page>
2024-08-29 19:26:41 +0600

    Update Credit.tsx

--- src/app/components/Credit.tsx
+++ src/app/components/Credit.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 
 const Card: React.FC = () => {
   return (
-    <div className="fixed right-0 top-1/2 transform -translate-y-1/2 w-80 h-80 rounded-l-none rounded-r-lg shadow-lg bg-white overflow-hidden">
+    <div className="fixed right-0 top-1/2 transform -translate-y-1/2 w-80 h-80 rounded-r-lg shadow-lg bg-white overflow-hidden">
       <div className="relative w-full h-full">
         <Image
           src="/dp.webp"
           alt="সাঈদ"
           width={100}
           height={100}
-          className="object-cover w-full h-full"
+          className="absolute bottom-4 left-4 rounded-full"
         />
-        <div className="absolute inset-0 flex flex-col justify-center items-end pr-4 bg-gradient-to-l from-black/50 to-transparent">
-          <div className="writing-vertical-lr text-lg font-semibold text-white">
-            <p className="whitespace-nowrap text-yellow-300">সাঈদ</p>
-            <p className="whitespace-nowrap text-green-300">❤️ দিয়ে বানানো। Made in Cumilla</p>
+        <div className="absolute inset-0 flex flex-col justify-center items-end pr-4">
+          <div className="transform -rotate-90 text-lg font-semibold text-black">
+            <p className="whitespace-nowrap">সাঈদ</p>
+            <p className="whitespace-nowrap">❤️ দিয়ে বানানো। Made in Cumilla</p>
           </div>
         </div>
       </div>
