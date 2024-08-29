1748ad70542321a854b78e08f2d5c63c3f6ec532
MD. ABU SAYED <hello@sayed.page>
2024-08-29 20:24:13 +0600

    Update Credit.tsx

--- src/app/components/Credit.tsx
+++ src/app/components/Credit.tsx
@@ -2,28 +2,18 @@ import React from 'react';
 
 const Card: React.FC = () => {
   return (
-    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-85 p-4 rounded-t-lg rounded-b-none shadow-lg bg-white flex items-center">
-      <div className="flex">
-        <div className="flex justify-center items-center pr-4">
+    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-80 p-4 rounded-t-lg rounded-b-none shadow-lg bg-white flex items-center">
+      <div className="flex w-full">
+        <div className="w-1/4 flex justify-center items-center">
           <img
             src="/dp.webp"
             alt="সাঈদ"
             className="w-12 h-12 rounded-full"
           />
         </div>
-        <div className="flex flex-col justify-center">
-          <a
-            href="https://sayed.page/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm font-semibold text-black text-center"
-          >
-            সাঈদ
-          </a>
-          <p className="text-xs text-black text-center">
-            ❤️ দিয়ে বানানো হইছে। <br />
-            Made in Cumilla🇧🇩
-          </p>
+        <div className="w-3/4 flex flex-col justify-center">
+          <p className="text-lg font-semibold text-black text-center">সাঈদ</p>
+          <p className="text-lg text-black text-center">❤ দিয়া বানানো হইছে!</p>
         </div>
       </div>
     </div>
