f6538e28c5292bae4e618a5e842134c28e2b5621
MD. ABU SAYED <hello@sayed.page>
2024-08-29 20:10:48 +0600

    Update Credit.tsx

--- src/app/components/Credit.tsx
+++ src/app/components/Credit.tsx
@@ -1,22 +1,19 @@
-import Image from 'next/image';
 import React from 'react';
 
 const Card: React.FC = () => {
   return (
-    <div className="fixed bottom-0 top-1/2 transform -translate-y-1/2 w-80 h-80 rounded-l-lg shadow-lg bg-white overflow-hidden flex flex-col items-center">
-      <div className="w-full h-40 flex justify-center items-center">
-        <Image
-          src="https://sayed.page/dp.webp"
-          alt="সাঈদ"
-          width={100}
-          height={100}
-          className="rounded-full"
-        />
-      </div>
-      <div className="flex-1 flex items-center justify-center">
-        <div className="transform text-lg font-semibold text-black">
-          <p className="whitespace-nowrap text-center">সাঈদ</p>
-          <p className="whitespace-nowrap text-center">❤ দিয়া বানানো হইছে!</p>
+    <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-80 p-4 rounded-t-lg rounded-b-none shadow-lg bg-white flex items-center">
+      <div className="flex w-full">
+        <div className="w-1/4 flex justify-center items-center">
+          <img
+            src="/dp.webp"
+            alt="সাঈদ"
+            className="w-12 h-12 rounded-full"
+          />
+        </div>
+        <div className="w-3/4 flex flex-col justify-center">
+          <p className="text-lg font-semibold text-black text-center">সাঈদ</p>
+          <p className="text-lg text-black text-center">❤ দিয়া বানানো হইছে!</p>
         </div>
       </div>
     </div>
