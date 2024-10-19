4645181bcbe19eeeb67877c33699527c45f2d1f5
MD. ABU SAYED <hello@sayed.page>
2024-10-19 11:53:26 +0000

    Revert "Auth"
    This reverts commit 2470cffa3626989b08428fa90f5b36027a5266b3.

--- src/app/auth/confirm/route.ts
+++ src/app/auth/confirm/route.ts
@@ -26,4 +26,3 @@ export async function GET(request: NextRequest) {
   // redirect the user to an error page with some instructions
   redirect('/error')
 }
-export const runtime = 'edge';
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -12,4 +12,3 @@ export default async function PrivatePage() {
 
   return <p>Hello {data.user.email}</p>
 }
-export const runtime = 'edge';
