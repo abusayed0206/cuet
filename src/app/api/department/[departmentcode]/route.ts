5187bd95f67627d244f45066804ddc85b7640b01
MD. ABU SAYED <hello@sayed.page>
2024-10-27 10:45:17 +0600

    Update route.ts

--- src/app/api/department/[departmentcode]/route.ts
+++ src/app/api/department/[departmentcode]/route.ts
@@ -13,7 +13,8 @@ const departmentMap: { [key: string]: string } = {
   "bme": "Department of Biomedical Engineering",
   "mie": "Department of Mechatronics & Industrial Engineering",
   "wrp": "Department of Water Resources Engineering",
-  "mse": "Department of Materials Science & Engineering"
+  "mse": "Department of Materials Science & Engineering",
+  "mme": "Department of Materials and Metallurgical Engineering"
 };
 
 export async function GET(
