80f6b746ab3546b9c9c77696dbbc9321fb125a86
MD. ABU SAYED <hello@sayed.page>
2025-07-25 02:47:25 +0600

    tailwind updated to v4

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    R2_ACCOUNT_ID: process.env.R2_ACCOUNT_ID,
-    R2_ACCESS_KEY_ID: process.env.R2_ACCESS_KEY_ID,
-    R2_SECRET_ACCESS_KEY: process.env.R2_SECRET_ACCESS_KEY,
-    R2_BUCKET: process.env.R2_BUCKET,
-  },
   async rewrites() {
     return [
       {
